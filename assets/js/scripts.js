042fbf8eab65ed3f70a2ca060c1c43a0714c082e
Davi Blasioli <daviblasioli15@gmail.com>
2025-02-18 16:29:00 -0300

    home pornta

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,3 +15,19 @@ $(document).ready(function(){
             }
         }
 })  });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.getElementById("menuButton");
+    const dropdownMenu = document.getElementById("dropdownMenu");
+
+    menuButton.addEventListener("click", function() {
+        dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+    });
+
+    // Fecha o menu caso clique fora dele
+    document.addEventListener("click", function(event) {
+        if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.display = "none";
+        }
+    });
+});
