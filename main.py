eb8e8e721e45d3bd5db8daf1ce13566d9dc2d891
Kenzo Mitsu <heitor.mitsuuti@gmail.com>
2025-02-24 10:11:03 -0300

    python definitivo, não apagar

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    
+
+@app.route('/home')
+def
+
+@app.route('/login')
+def
+
+@app.route('/cadastro')
+def
