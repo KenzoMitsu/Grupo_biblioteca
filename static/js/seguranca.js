61ec7f5cb315538d5a54dab6292b828fa608ff5e
Tilangaa <pepedoolima@gmail.com>
2025-05-05 11:26:27 -0300

    headers concertados

--- static/js/seguranca.js
+++ static/js/seguranca.js
@@ -29,7 +29,7 @@ if (cargo === 'ADM') {
 
         <li class="menu-item">
             <div>
-                <i class="fa-solid fa-circle-user"></i><a id="editPerfil" href="editPerfil.html"> Perfil</a>
+                <i class="fa-solid fa-circle-user fa-lg"></i><a id="editPerfil" href="editPerfil.html"> Perfil</a>
             </div>
         </li>
 
@@ -110,7 +110,7 @@ if (cargo === 'ADM') {
 
         <li class="menu-item">
             <div>
-                <i class="fa-solid fa-circle-user"></i><a id="editPerfil" href="editPerfil.html"> Perfil</a>
+                <i class="fa-solid fa-circle-user fa-lg"></i><a id="editPerfil" href="editPerfil.html"> Perfil</a>
             </div>
         </li>
 
@@ -139,7 +139,7 @@ if (cargo === 'ADM') {
 
         <li class="menu-item">
             <div>
-                <i class="fa-solid fa-circle-user"></i><a id="editPerfil" href="editPerfil.html"> Perfil</a>
+                <i class="fa-solid fa-circle-user fa-lg"></i><a id="editPerfil" href="editPerfil.html"> Perfil</a>
             </div>
         </li>
 
