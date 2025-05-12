c82073d3504a960c508347c680b9dff39fde2b99
Davi Blasioli <daviblasioli15@gmail.com>
2025-05-12 13:38:52 -0300

    ip

--- static/js/paginaip.js
+++ static/js/paginaip.js
@@ -1,5 +1,5 @@
-const ipPython = "http://127.0.0.1:5000"
-const ipPython2 = "http://127.0.0.1:5000"
+const ipPython = "http://192.168.1.137:5000"
+const ipPython2 = "http://192.168.1.137:5000"
 
 // ESSA PÁGINA É ONDE FICA SALVA A VARIÁVEL RESPONSÁVEL POR
 // ARMAZENAR O URL DO IP DO SERVIDOR
