2894b2d5ab50d538c68c28c331d8cca8e4a2c785
Ana Clara <167575344+davicblasioli@users.noreply.github.com>
2025-06-02 13:32:05 -0300

    ip

--- static/js/paginaip.js
+++ static/js/paginaip.js
@@ -1,5 +1,5 @@
-const ipPython  = "http://192.168.1.147:5000"
-const ipPython2 = "http://192.168.1.147:5000"
+const ipPython  = "http://10.92.3.149:5000"
+const ipPython2 = "http://10.92.3.149:5000"
 
 // ESSA PÁGINA É ONDE FICA SALVA A VARIÁVEL RESPONSÁVEL POR
 // ARMAZENAR O URL DO IP DO SERVIDOR
